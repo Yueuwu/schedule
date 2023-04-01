463066d66aab4a07337e7e11642cab0bb117d7b1
Yueuwu <vladdenega1@gmail.com>
2023-04-01 13:34:16 +0700

    get hurt from backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import './App.css';
 import {useAppDispatch, useAppSelector} from "./app/hooks";
 import {getSchedule, scheduleSelector} from "./features/schedule/scheduleSlice";
+import Day from "./components/day/Day";
 
 function App() {
   const dispatch = useAppDispatch()
@@ -9,11 +10,11 @@ function App() {
     dispatch(getSchedule())
   }, [])
   const {value, status} = useAppSelector(scheduleSelector)
-  console.log('status',status)
-  console.log(value);
   return (
     <div className="App">
-
+      {
+        value["Понедельник "] && <Day props={value["Понедельник "]}/>
+      }
     </div>
   );
 }
---
+++ src/components/day/Day.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Day: React.FC<{props: any}> = (props) => {
+    console.log(props);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Day;
