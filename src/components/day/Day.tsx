1f339dec12f00647a26150906793e22c3ed30d76
Yueuwu <vladdenega1@gmail.com>
2023-04-16 14:04:58 +0700

    day component updated

--- src/components/day/Day.tsx
+++ src/components/day/Day.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import Lesson from "../lesson/Lesson";
+import style from './day.module.css'
 
-const Day: React.FC<{props: any}> = (props) => {
-    console.log(props);
+const Day: React.FC<{day: any}> = ({day}) => {
+    const dayChanger = (day: any) => {
+        switch (day) {
+            case "Monday": return "Понедельник";
+            case "Tuesday": return "Вторник";
+            case "Wednesday": return "Среда";
+            case "Thursday": return "Четверг";
+            case "Friday": return "Пятница";
+            case "Saturday": return "Суббота";
+        }
+    }
     return (
-        <div>
-
+        <div className={style.wrapper}>
+            <div className={style.day}>
+                <p>{dayChanger(day.day)}</p>
+            </div>
+            <div className={style.lessons}>
+                {day.object.map((lesson: any) => <Lesson lesson={lesson}/>)}
+            </div>
         </div>
     );
 };
